38081c36482f690099f54d9f0116470edf85c047
DavePhil <dave.chedjoun@facsciences-uy1.cm>
2023-06-04 13:59:50 +0100

    Adding Jenkins and docker

--- src/main/java/com/foft/microserviceenseignant/controller/EnseignantController.java
+++ src/main/java/com/foft/microserviceenseignant/controller/EnseignantController.java
@@ -1,7 +1,26 @@
 package com.foft.microserviceenseignant.controller;
 
+import com.foft.microserviceenseignant.modele.Enseignant;
+import com.foft.microserviceenseignant.service.EnseignantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+
+@RestController
+@RequestMapping("MicroEnseignant")
 public class EnseignantController {
+
+    @Autowired
+    private EnseignantService enseignantService;
+    @GetMapping("/ListEnseignant")
+    public List<Enseignant> allEnseigant()
+    {
+        return enseignantService.findAll();
+    }
 }
--- src/main/java/com/foft/microserviceenseignant/service/EnseignantService.java
+++ src/main/java/com/foft/microserviceenseignant/service/EnseignantService.java
@@ -1,9 +1,19 @@
 package com.foft.microserviceenseignant.service;
 
+import com.foft.microserviceenseignant.modele.Enseignant;
+import com.foft.microserviceenseignant.repository.EnseignantRepository;
 import lombok.Data;
+import org.bouncycastle.crypto.ec.ECNewRandomnessTransform;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Data
 @Service
 public class EnseignantService {
+    @Autowired
+    EnseignantRepository enseignantRepository;
+    public List<Enseignant> findAll(){
+        return enseignantRepository.findAll();    }
 }
