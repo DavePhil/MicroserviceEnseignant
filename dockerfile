76d41c9ddad7f305c52e9a006e48b0061da2b6e4
DavePhil <dave.chedjoun@facsciences-uy1.cm>
2023-06-04 23:46:10 +0100

    Adding Jenkins and docker

--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:8
 WORKDIR /app
-
+EXPOSE 9009
 COPY target/microenseignant.jar /app
 CMD ["java","-jar","microenseignant.jar"]
--- src/main/java/com/foft/microserviceenseignant/controller/EnseignantController.java
+++ src/main/java/com/foft/microserviceenseignant/controller/EnseignantController.java
@@ -3,8 +3,6 @@ package com.foft.microserviceenseignant.controller;
 import com.foft.microserviceenseignant.modele.Enseignant;
 import com.foft.microserviceenseignant.service.EnseignantService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
